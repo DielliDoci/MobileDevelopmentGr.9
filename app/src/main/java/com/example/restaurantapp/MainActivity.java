52f9b823a319fdfbc4ca59419a555f1287c9453b
DielliDoci <dielli.doci@student.uni-pr.edu>
2024-12-01 16:06:06 +0100

    profile page + some UI changes

--- app/src/main/java/com/example/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.restaurantapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,31 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        TextView welcomeText = findViewById(R.id.welcomeText);
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.welcome_animation);
-        welcomeText.startAnimation(animation);
-
-
-        Button btnSignIn = findViewById(R.id.btnSignIn);
-        Button btnSignUp = findViewById(R.id.btnSignUp);
-
-
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        ImageView profileIcon = findViewById(R.id.profileIcon);
+        profileIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
             }
         });
     }
 }
+
+
+
