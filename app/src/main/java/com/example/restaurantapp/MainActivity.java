6d7e5eecc6eba3b16529243aba62b0b878cd39ae
DielliDoci <dielli.doci@student.uni-pr.edu>
2024-12-01 08:34:26 +0100

    welcome page starter

---
+++ app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.restaurantapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.welcome_animation);
+        welcomeText.startAnimation(animation);
+
+
+        Button btnSignIn = findViewById(R.id.btnSignIn);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
